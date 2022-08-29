4c991f7ab8cea0d12604f11a32dd289392f97955
Steven Eschrich <35569991+steveneschrich@users.noreply.github.com>
2022-08-29 10:11:02 -0400

    Added wrapper function and included a few parameters in summarize_followup to make the choices of group headers and follow up statistic (counts, etc) controllable.

--- R/summarize_followup.R
+++ R/summarize_followup.R
@@ -1,30 +1,61 @@
-#' Create a categorical table with followup comments
+#' Title
 #'
 #' @param x
-#' @param var
-#' @param followup
-#' @param header
+#' @param ...
 #'
 #' @return
+#' @export
+#'
+#' @examples
+followup_flextable <- function(x, ...) {
+  summarize_followup(x, ...) |>
+    style_followup_as_flextable()
+}
+
+#' Create a categorical table with followup comments
+#'
+#' @param x Labelled data frame containing all data in survey
+#' @param var Variable to include as frequency table (character)
+#' @param followup Variable to include as follow-up field (character)
+#' @param header
+#' @param group_headers A vector of strings of header names for the questions (default
+#'  QUESTION) and followup (default FOLLOWUP).
+#' @param followup_statistic A statistic (see [gtsummary::tbl_summary()]). Default
+#'   is `"{n}"`, meaning include counts.
+#'
+#' @return A [gtsummary::tbl_summary()] representing the frequency of `var`, with
+#'  any additional free text responses in `followup` listed below the table.
+#'
 #' @export
 #' @importFrom magrittr %>%
 #'
 #' @examples
-summarize_followup <- function(x, var, followup, header="**Characteristic**") {
+summarize_followup <- function(x, var, followup, header="**Characteristic**",
+                               group_headers = c("QUESTION","FOLLOWUP"),
+                               followup_statistic = "{n}") {
 
-  cattable<-dplyr::select(x, dplyr::all_of(var)) %>%
+  # If embedded lists are present, expand them here.
+  cattable <- dplyr::select(x, dplyr::all_of(var)) |>
+    expand_embedded_list()
+
+
+  cattable<-cattable |>
     gtsummary::tbl_summary(type=list(gtsummary::everything() ~ "categorical"))
   ftable <- dplyr::select(x, dplyr::all_of(followup)) %>%
     dplyr::na_if("") %>%
-    gtsummary::tbl_summary(statistic = list(gtsummary::all_categorical() ~ ""),
-                           missing = "no")
+    gtsummary::tbl_summary(
+      statistic = list(gtsummary::all_categorical() ~ followup_statistic),
+      missing = "no"
+    )
 
   gtsummary::tbl_stack(list(cattable, ftable), quiet = TRUE,
-                       group_header = c("QUESTION", "FOLLOWUP")) %>%
+                       group_header = group_headers) %>%
     gtsummary::modify_header(update = list(label ~ header, groupname_col ~ "")) %>%
     gtsummary::modify_footnote(update = gtsummary::everything() ~ NA)
 
 }
+
+
 
 #' Convert followup table to flextable output
 #'
