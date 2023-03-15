74c0dc518bc96e2ea41d2bb62f468812e1441ad2
Steven Eschrich <35569991+steveneschrich@users.noreply.github.com>
2023-03-15 13:39:58 -0400

    Mega-commit of refactored code for summarize/flextable. Added in new types as well.

---
+++ R/summarize_categorical_followup.R
@@ -0,0 +1,113 @@
+#' Categorical Followup data type
+#'
+#' @description A categorical followup type is a question involving one response out of
+#' a set of options. These options can be treated as numeric if on a likert
+#' scale, but generally are categorical. The followup is a question like "Other, please specify"
+#'
+#'
+#' @details With a categorical variable, the summarizations are frequency
+#' tables by category level.
+#'
+#' Possibilities include
+#' - summarize_categorical_followup: Create a summarized table of the question(s) and append the
+#' followup responses.
+#' - plot_categorical_followup: Create a figure of the categorical response frequencies (no
+#' followup responses).
+#' - gvsummary_categorical_followup: Create a flextable and plot of the categorical responses.
+#'
+#' @name categorical_followup
+NULL
+
+
+#' Create a categorical table with followup comments
+#'
+#' @param x Labelled data frame containing all data in survey
+#' @param vars (default: dplyr::everything) Columns of the data frame
+#'    that contain categorical fields
+#' @param by Variable to stratify summary by.
+#' @param followup A column to use as the follow up question/response (comment).
+#' @param followup_statistic A statistic (see [gtsummary::tbl_summary()]). Default
+#'   is `"{n}"`, meaning include counts.
+#' @param header The overall header for the table
+#' @param group_headers A vector of strings of header names for the questions (default
+#'  QUESTION) and followup (default FOLLOWUP).
+#' @param include_overall (TRUE) Include a column of overall counts. Only useful when
+#'  stratifying (`by`).
+#' @param output A string representing the desired output. Choices include:
+#'  - "gtsummary" A [gtsummary::gtsummary] object.
+#'  - "flextable" A [flextable::flextable] table with formatting applied to a gtsummary table.
+#'  - "rmarkdown" Raw flextable output for use in rmarkdown (see [flextable::flextable_to_rmd()]
+#'    for details). This is most suitable for running summarize in a loop or as list component.
+#'
+#' @return A table representing the frequency of `vars`, with
+#'  any additional free text responses in `followup` listed below the table.
+#'
+#' @export
+#'
+summarize_categorical_followup <- function(x, vars, by = NULL,
+                                           followup,followup_statistic = "{n}",
+                                           header="**Characteristic**",
+                                           group_headers = c("",""),
+
+                               include_overall = FALSE,
+                               output = c("gtsummary","flextable", "rmarkdown")
+                               ) {
+
+  output <- match.arg(output)
+  cattable<-summarize_categorical(
+    x,
+    vars = vars,
+    include_overall=include_overall,
+    by = by,
+    output = "gtsummary"
+  )
+
+  ftable <- summarize_comments(
+    x,
+    vars = followup,
+    include_counts = TRUE,
+    by = by,
+    include_overall = include_overall,
+    output = "gtsummary",
+    statistic = followup_statistic
+  )
+
+  tbl <- gtsummary::tbl_stack(list(cattable, ftable), quiet = TRUE,
+                       group_header = group_headers)
+
+  # Decide on output: gtsummary, flextable or rmarkdown.
+  if ( output == "flextable" || output == "rmarkdown") {
+    tbl <- gtsummary::as_flex_table(tbl) |>
+      evrt::style_categorical_followup_flextable()
+    if ( output == "rmarkdown" )
+      tbl <- flextable::flextable_to_rmd(tbl)
+  }
+
+  tbl
+
+}
+
+
+
+#' Style flextable as a followup table
+#'
+#' @description Apply consistent styling to a follow up flextable
+#'  to fit the typical contents of the table.
+#'
+#' @param x A flextable
+#'
+#' @return A flextable with specific styling applied
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' gtsummary::tbl_summary(iris) |>
+#'   as_flextable() |>
+#'   style_followup_flextable()
+#' }
+style_categorical_followup_flextable <- function(x) {
+  x |>
+    flextable::set_table_properties(layout="autofit", width=1) |>
+    #flextable::width(j=1:6,width=c(1,3,0.75,0.75,0.75,0.75)) %>%
+    style_flextable()
+}
--- R/summarize_numeric.R
+++ R/summarize_numeric.R
@@ -1,17 +1,19 @@
-#' Title
+#' Numeric data type
 #'
-#' @param x
-#' @param ...
+#' @description A numeric type is a question involving a numeric output. These
+#' can be options that can be treated as numeric if on a likert
+#' scale, but can also be general numeric values.
 #'
-#' @return
-#' @export
+#' @details With a numeric variable, the summarizations are mean/median, etc
 #'
-#' @examples
-flextable_numeric <- function(x, ...) {
-  summarize_numeric(x, ...) |>
-    as_flextable() |>
-    style_numeric_flextable()
-}
+#'
+#' Possibilities include
+#' - summarize_numeric: Create a summarized table of the question(s)
+#' - plot_numeric: Create a figure of the numeric responses
+#' - gvsummary_numeric: Create a flextable and plot of the numeric responses.
+#'
+#' @name categorical
+NULL
 
 
 
@@ -20,11 +22,21 @@ flextable_numeric <- function(x, ...) {
 #'
 #' @param x A data frame to summarize
 #' @param vars Specific variables to summarize (default is all).
+#' @param by Variable to stratify summary by.
 #' @param dictionary The data dictionary for the instrument (optional)
-#' @param header
-#' @param summary
+#' @param header (default: "**Characteristic**") The summary table header
+#' @param summary A summary measure (default = "mean")
+#' @param include_overall Include a column of overall summary. Note this only
+#'   works (and makes sense) when the `by` variable is used.
+#' @param output A string representing the desired output. Choices include:
+#'  - "gtsummary" A [gtsummary::gtsummary] object.
+#'  - "flextable" A [flextable::flextable] table with formatting applied to a gtsummary table.
+#'  - "rmarkdown" Raw flextable output for use in rmarkdown (see [flextable::flextable_to_rmd()]
+#'    for details). This is most suitable for running summarize in a loop or as list component.
 #'
-#' @return
+#' @return A table object representing the numeric
+#'  breakdown of `vars` from `x`.
+#
 #' @export
 #'
 #' @examples
@@ -32,25 +44,30 @@ flextable_numeric <- function(x, ...) {
 #' summarize_numeric(iris, vars=c("Petal.Length","Petal.Width"))
 #' }
 summarize_numeric <- function(x,
-                              vars = colnames(x), dictionary = NULL,
+                              vars = colnames(x),
+                              by=NULL,
+                              dictionary = NULL,
                               header = "**Characteristic**", summary="mean",
-                              by=NULL) {
+                              include_overall = TRUE,
+                              output = c("gtsummary","flextable", "rmarkdown")) {
 
+  output <- match.arg(output)
   # Get factor levels and convert everything to numeric (if needed).
   factor_levels <- get_factor_levels(x, vars, dictionary)
-  if ( is.null(dictionary) ) {
-    x <- x |>
-      dplyr::mutate(dplyr::across(
-        dplyr::all_of(vars),
-        \(.x){
-          as.numeric(.x)
-        }
-      ))
-  }
-  gtsummary::tbl_summary(
+
+  #if ( is.null(dictionary) ) {
+  #  x <- x |>
+  #    dplyr::mutate(dplyr::across(
+  #      dplyr::all_of(vars),
+  #      \(.x){
+  #        as.numeric(.x)
+  #      }
+  #    ))
+  #}
+  tbl <- gtsummary::tbl_summary(
     x,
     include=dplyr::all_of(vars),
-    by= by,
+    by= dplyr::all_of(by),
     type = list(everything() ~ "continuous"),
     statistic = list(everything() ~ ifelse(
       summary=="mean",
@@ -64,6 +81,20 @@ summarize_numeric <- function(x,
     gtsummary::modify_footnote(label~glue::glue(
       "Levels: {paste(names(factor_levels),factor_levels, sep='=', collapse=', ')}"
     ))
+
+  # Add overall if needed
+  if ( !is.null(by) && include_overall )
+    tbl <- gtsummary::add_overall(tbl, last = TRUE, col_label="**Total** N = {N}")
+
+  # Decide on output: gtsummary, flextable or rmarkdown.
+  if ( output == "flextable" || output == "rmarkdown") {
+    tbl <- gtsummary::as_flex_table(tbl) |>
+      evrt::style_numeric_flextable()
+    if ( output == "rmarkdown" )
+      tbl <- flextable::flextable_to_rmd(tbl)
+  }
+
+  tbl
 }
 
 
